fbe6606c932d39483b077ce04b60accbe44036c6
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-06-30 02:37:25 +0300

    add Repository note class

---
+++ Customer/Repositories/NoteRepository.cs
@@ -0,0 +1,121 @@
+﻿using System.Data.SqlClient;
+namespace CustomerLib.Repositories
+{
+    public class NoteRepository
+    {
+        public void Create(string note, int customerIdx)
+        {
+            using (var connection = new SqlConnection("Server=ALFA;Database=CustomerLib_Bezslyozniy;Trusted_Connection=True;"))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("INSERT INTO [dbo].[Notes] (CustomerID, Note)" +
+                    "VALUES(@CustomerID, @Note)", connection);
+                var noteCustomerIDParam = new SqlParameter("CustomerID", System.Data.SqlDbType.Int)
+                {
+                    Value = customerIdx
+                };
+
+                var noteNoteParam = new SqlParameter("@Note", System.Data.SqlDbType.VarChar, 500)
+                {
+                    Value = note
+                };
+
+                command.Parameters.Add(noteCustomerIDParam);
+                command.Parameters.Add(noteNoteParam);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(string note, int customerIdx, int noteIdx)
+        {
+
+            using (var connection = new SqlConnection("Server=ALFA;Database=CustomerLib_Bezslyozniy;Trusted_Connection=True;"))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("UPDATE [dbo].[Notes] SET Note = @Note", connection);
+                var noteNoteParam = new SqlParameter("Note", System.Data.SqlDbType.VarChar, 500)
+                {
+                    Value = note
+                };
+
+                command.Parameters.Add(noteNoteParam);
+                
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int customerId, int noteId)
+        {
+            using (var connection = new SqlConnection("Server=ALFA;Database=CustomerLib_Bezslyozniy;Trusted_Connection=True;"))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("DELETE FROM [Notes] WHERE CustomerID = @CustomerID AND NoteID = @NoteID", connection);
+
+                var noteCustomerIDParam = new SqlParameter("CustomerID", System.Data.SqlDbType.Int)
+                {
+                    Value = customerId
+                };
+                var noteNoteIDParam = new SqlParameter("NoteID", System.Data.SqlDbType.Int)
+                {
+                    Value = noteId
+                };
+
+                command.Parameters.Add(noteCustomerIDParam);
+                command.Parameters.Add(noteNoteIDParam);
+
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public string Read(int customerId, int noteId)
+        {
+            using (var connection = new SqlConnection("Server=ALFA;Database=CustomerLib_Bezslyozniy;Trusted_Connection=True;"))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT * FROM [Notes] WHERE CustomerID = @customerId AND NoteID = @NoteID", connection);
+
+                var noteCustomerIDParam = new SqlParameter("CustomerID", System.Data.SqlDbType.Int)
+                {
+                    Value = customerId
+                };
+                var noteNoteIDParam = new SqlParameter("NoteID", System.Data.SqlDbType.Int)
+                {
+                    Value = noteId
+                };
+
+                command.Parameters.Add(noteCustomerIDParam);
+                command.Parameters.Add(noteNoteIDParam);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                        return reader["Note"]?.ToString();
+                }
+
+                command.ExecuteNonQuery();
+            }
+
+            return null;
+        }
+
+        public void DeleteAll()
+        {
+            using (var connection = new SqlConnection("Server=ALFA;Database=CustomerLib_Bezslyozniy;Trusted_Connection=True;"))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("DELETE FROM [Addresses]", connection);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+    }
+}
