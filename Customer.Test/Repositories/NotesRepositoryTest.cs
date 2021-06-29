06c431400a9c3d348a2c5b95d33fcec9d7a86a6f
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-06-30 02:37:03 +0300

    add Repository note test

---
+++ Customer.Test/Repositories/NotesRepositoryTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using CustomerLib.Repositories;
+namespace CustomerLib.Test.Repositories
+{
+    public class NotesRepositoryTest
+    {
+        [Fact]
+        public void ShouldBeAbleToCreateNotesRepository()
+        {
+            var notesRepository = new NoteRepository();
+            Assert.NotNull(notesRepository);
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToCreateNotes()
+        {
+            var notesRepository = new NoteRepository();
+
+            var note = "NOTE";
+            notesRepository.Create(note, 1);
+
+            var createdNote = notesRepository.Read(1, 3);
+            Assert.NotNull(createdNote);
+
+            Assert.Equal("NOTE", createdNote);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToReadNotes()
+        {
+            var noteRepository = new NoteRepository();
+
+            var readedNote = noteRepository.Read(1, 3);
+            Assert.NotNull(readedNote);
+
+            Assert.Equal("NOTE", readedNote);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToUpdateNotes()
+        {
+            var noteRepository = new NoteRepository();
+            Assert.Equal("NOTE", noteRepository.Read(1, 3));
+            
+            var note = "UPDATED NOTE";
+            noteRepository.Update(note, 1, 3);
+
+            Assert.Equal("UPDATED NOTE", noteRepository.Read(1, 3));
+        }
+
+        [Fact]
+        public void ShouldBeAbleToDeleteNote()
+        {
+            var noteRepository = new NoteRepository();
+            int noteId = 1, customerId = 1;
+
+            noteRepository.Delete(customerId, noteId);
+            Assert.Null(noteRepository.Read(customerId, noteId));
+        }
+    }
+}
