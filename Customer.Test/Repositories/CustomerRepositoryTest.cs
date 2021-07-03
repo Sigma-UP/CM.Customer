ce96f4a51c22950bd4b403dc5134032d35329ca5
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-07-03 16:48:16 +0300

    added fixture class

--- Customer.Test/Repositories/CustomerRepositoryTest.cs
+++ Customer.Test/Repositories/CustomerRepositoryTest.cs
@@ -12,98 +12,71 @@ namespace CustomerLib.Test.Repositories
             Assert.NotNull(customerRepository);
         }
 
-
         [Fact]
         public void ShouldBeAbleToCreateCustomer()
         {
             var customerRepository = new CustomerRepository();
-            customerRepository.DeleteAll();
-            customerRepository.Create(new Customer()
-            {
-                FirstName = "Alex",
-                LastName = "Xlea",
-                Addresses = new List<Address>() {
-                        new Address(){
-                        Line1 = "CustomerLine1",
-                        Line2 = "CustomerLine2",
-                        AddressType = Address.EAddressType.Shipping,
-                        City = "Denver",
-                        PostalCode = "121212",
-                        State = "State",
-                        Country = "Canada" }
-                    },
-                Notes = new List<string>() {
-                        "Customer Note"
-                    },
-                Phone = "+36098983443",
-                Email = "myname@gmail.com",
-                TotalPurchasesAmount = (decimal?)1234.3
-            });
+            var fixture = new CustomerRepositoryFixture();
+            var customer = fixture.CreateMockCustomer();
 
             var createdCustomer = customerRepository.Read(1);
-            Assert.NotNull(createdCustomer);
-
-            Assert.Equal("Alex", createdCustomer.FirstName);
-            Assert.Equal("Xlea", createdCustomer.LastName);
-
-            Assert.Equal(Address.EAddressType.Shipping, createdCustomer.Addresses[0].AddressType);
-            Assert.Equal("Denver", createdCustomer.Addresses[0].City);
-            Assert.Equal("121212", createdCustomer.Addresses[0].PostalCode);
-
-            Assert.Equal("Customer Note", createdCustomer.Notes[0]);
-
+            fixture.Equal(customer, createdCustomer);
         }
 
         [Fact]
         public void ShouldBeAbleToReadCustomer()
         {
             var customerRepository = new CustomerRepository();
-            customerRepository.DeleteAll();
-
-            customerRepository.Create(new Customer()
-            {
-                FirstName = "Alex",
-                LastName = "Xlea",
-                Addresses = new List<Address>() {
-                        new Address(){
-                        Line1 = "CustomerLine1",
-                        Line2 = "CustomerLine2",
-                        AddressType = Address.EAddressType.Shipping,
-                        City = "Denver",
-                        PostalCode = "121212",
-                        State = "State",
-                        Country = "Canada" }
-                    },
-                Notes = new List<string>() {
-                        "Customer Note"
-                    },
-                Phone = "+36098983443",
-                Email = "myname@gmail.com",
-                TotalPurchasesAmount = (decimal?)1234.3
-            });
-
+            var fixture = new CustomerRepositoryFixture();
+            var customer = fixture.CreateMockCustomer();
             var readedCustomer = customerRepository.Read(1);
-
-            Assert.NotNull(readedCustomer);
-
-            Assert.Equal("Alex", readedCustomer.FirstName);
-            Assert.Equal("Xlea", readedCustomer.LastName);
-
-            Assert.Equal(Address.EAddressType.Shipping, readedCustomer.Addresses[0].AddressType);
-            Assert.Equal("Denver", readedCustomer.Addresses[0].City);
-            Assert.Equal("121212", readedCustomer.Addresses[0].PostalCode);
-
-            Assert.Equal("Customer Note", readedCustomer.Notes[0]);
-
+            fixture.Equal(customer, readedCustomer);
         }
 
         [Fact]
         public void ShouldBeAbleToUpdateAddress()
         {
             var customerRepository = new CustomerRepository();
+            var fixture = new CustomerRepositoryFixture();
+            var customer = fixture.CreateMockCustomer();
+
+            customer.FirstName = "Mikhail";
+            customerRepository.Update(customer, 1);
+            
+            var readedCustomer = customerRepository.Read(1);
+            fixture.Equal(customer, readedCustomer);
+        }
+
+        [Fact]
+        public void ShouldBeAbleToDeleteCustomer()
+        {
+            var customerRepository = new CustomerRepository();
+            var fixture = new CustomerRepositoryFixture();
+            var customer = fixture.CreateMockCustomer();
+
+            Assert.NotNull(customerRepository.Read(1));
+
+            customerRepository.Delete(1);
+
+            Assert.Null(customerRepository.Read(1));
+        }
+    }
+    public class CustomerRepositoryFixture
+    {
+        public Customer CreateMockCustomer()
+        {
+            var customerRepository = new CustomerRepository();
             customerRepository.DeleteAll();
 
-            var customer = new Customer()
+            var customer = MockCustomer();
+            customerRepository.Create(customer);
+
+            return customer;
+        }
+
+        public Customer MockCustomer()
+        {
+            return new Customer()
             {
                 FirstName = "Alex",
                 LastName = "Xlea",
@@ -124,49 +97,23 @@ namespace CustomerLib.Test.Repositories
                 Email = "myname@gmail.com",
                 TotalPurchasesAmount = (decimal?)1234.3
             };
-            customerRepository.Create(customer);
-            customer.FirstName = "Mikhail";
-            customerRepository.Update(customer, 1);
-
-            var updatedCustomer = customerRepository.Read(1);
-            Assert.NotNull(updatedCustomer);
-
-            Assert.Equal(customer.FirstName, updatedCustomer.FirstName);
-            Assert.Equal(customer.LastName, updatedCustomer.LastName);
-            Assert.Equal(customer.Phone, updatedCustomer.Phone);
         }
 
-        [Fact]
-        public void ShouldBeAbleToDeleteCustomer()
+        public void Equal(Customer customer, Customer readedCustomer)
         {
-            
-            var customerRepository = new CustomerRepository();
-            int customerId = 1;
-            customerRepository.DeleteAll();
-            customerRepository.Create(new Customer()
-            {
-                FirstName = "Alex",
-                LastName = "Xlea",
-                Addresses = new List<Address>() {
-                        new Address(){
-                        Line1 = "CustomerLine1",
-                        Line2 = "CustomerLine2",
-                        AddressType = Address.EAddressType.Shipping,
-                        City = "Denver",
-                        PostalCode = "121212",
-                        State = "State",
-                        Country = "Canada" }
-                    },
-                Notes = new List<string>() {
-                        "Customer Note"
-                    },
-                Phone = "+36098983443",
-                Email = "myname@gmail.com",
-                TotalPurchasesAmount = (decimal?)1234.3
-            });
-            customerRepository.Delete(customerId);
-            Assert.Null(customerRepository.Read(customerId));
+            Assert.NotNull(readedCustomer);
 
+            Assert.Equal(customer.FirstName, readedCustomer.FirstName);
+            Assert.Equal(customer.LastName, readedCustomer.LastName);
+            Assert.Equal(customer.Phone, readedCustomer.Phone);
+            Assert.Equal(customer.Email, readedCustomer.Email);
+            Assert.Equal(customer.TotalPurchasesAmount, readedCustomer.TotalPurchasesAmount);
+
+            Assert.Equal(customer.Addresses[0].AddressType, readedCustomer.Addresses[0].AddressType);
+            Assert.Equal(customer.Addresses[0].City, readedCustomer.Addresses[0].City);
+            Assert.Equal(customer.Addresses[0].PostalCode, readedCustomer.Addresses[0].PostalCode);
+
+            Assert.Equal(customer.Notes[0], readedCustomer.Notes[0]);
         }
     }
 }
