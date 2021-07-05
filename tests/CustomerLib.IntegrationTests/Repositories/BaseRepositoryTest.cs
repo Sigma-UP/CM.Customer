d1c0c0c7ae59e374649fd49d55eb9963bc3ae0e1
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-07-06 00:08:46 +0300

    add BaseRepository integration test

---
+++ tests/CustomerLib.IntegrationTests/Repositories/BaseRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using CustomerLib.Repositories;
+
+namespace CustomerLib.IntegrationTests.Repositories
+{
+    public class BaseRepositoryTest
+    {
+        [Fact]
+        public void ShouldBeAbleToReturnConnection()
+        {
+            Assert.NotNull(BaseRepository.GetConnection());
+            Assert.Equal("ALFA", BaseRepository.GetConnection().DataSource);
+            Assert.Equal("CustomerLib_Bezslyozniy", BaseRepository.GetConnection().Database);
+        }
+    }
+}
